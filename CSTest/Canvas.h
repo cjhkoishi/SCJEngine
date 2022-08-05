30ef2898ebb4351ac981dacad0d5ffb2220c51a7
薪辰 <jinhao.cjh@alibaba-inc.com>
2022-08-05 17:23:48 +0800

    wd

--- CSTest/Canvas.h
+++ CSTest/Canvas.h
@@ -9,8 +9,8 @@ public:
 	GLuint VBO, VAO, TBO;
 	ComputeShader kernel;
 	Shader shader;
-	bool is_running=false;
-	float a=10,b=28,c=3;
+	bool is_running = false;
+	float a = 10, b = 28, c = 3;
 
 	void start()override;
 	void render(const mat4& view, const mat4& proj)override;
@@ -21,8 +21,9 @@ class Canvas :public Renderer
 {
 	COMPONENT
 public:
-	GLuint VBO, VAO, EBO;
+	GLuint VBO, VAO, EBO, TEX0, TEX1;
 	Shader shader;
+	ComputeShader kernel;
 
 	void start();
 	void render(const mat4& view, const mat4& proj)override;

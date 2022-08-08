35a0f755cb57b65a323d3569c7f8331c96fe642d
jinhao chen <1905587166@qq.com>
2022-08-08 23:08:53 +0800

    5

--- SCJEngine/Renderer.cpp
+++ SCJEngine/Renderer.cpp
@@ -18,7 +18,7 @@ void RenderSystem::setFullWindow(GLFWwindow* window)
 
 void RenderSystem::run(GLFWwindow* window)
 {
-	glClearColor(0, 0, 0, 1);
+	glClearColor(0.1, 0.1, 0.1, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	setFullWindow(window);
 	mat4 view = render_area.getViewMat();
